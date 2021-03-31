fda21ae61b44bcb5f862eef53bdac49d64a8f666
Parth <parthshah576@gmail.com>
2021-03-31 19:44:21 +0530

    switch to structs for api response, change up structure of cachefile

--- cmd/search.go
+++ cmd/search.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2021 Parth Shah parthshah576@gmail.com
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -20,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"sort"
 	"strings"
 
 	"github.com/Parth576/gowords/colors"
@@ -28,27 +12,49 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Body struct {
+	Word     string    `json:"word"`
+	Meanings []Meaning `json:"meanings"`
+}
+
+type Meaning struct {
+	POS         string `json:"partOfSpeech" mapstructure:"partOfSpeech"`
+	Definitions []Def  `json:"definitions" mapstructure:"definitions"`
+}
+
+type Def struct {
+	Definition string `json:"definition" mapstructure:"definition"`
+	//Synonyms   []string `json:"synonyms"`
+	Example string `json:"example" mapstructure:"example"`
+}
+
 // searchCmd represents the search command
 var searchCmd = &cobra.Command{
 	Use:   "search",
 	Short: "Search definition for any word using an online dictionary, Example: gowords search hello",
 	Run: func(cmd *cobra.Command, args []string) {
-		if cacheSearch := viper.Get(args[0]); cacheSearch != nil {
-			//fmt.Println("Fetching definition from cache...")
-			for k, v := range cacheSearch.(map[string]interface{}) {
-				fmt.Printf("%s%s%s\n", colors.Cyan, strings.ToUpper(k), colors.Reset)
-				for _, def := range v.([]interface{}) {
-					if strings.HasPrefix(def.(string), "68f3fde1-8c1a-49eb-9f27-8d951b049142") {
-						fmt.Printf("%s\u2605%s%s%s\n", colors.Yellow, " ", colors.Reset, def.(string)[36:])
-					} else {
-						fmt.Printf("%s\u279C%s%s%s\n", colors.Blue, " ", colors.Reset, def)
-					}
-				}
-				fmt.Println()
-			}
+		if len(args) == 0 {
+			fmt.Println("Please provide word to be searched")
+		} else if len(args) > 1 {
+			fmt.Printf("Only 1 argument needed but %v were provided\n", len(args))
 		} else {
-			//fmt.Println("Fetching definition from API...")
-			searchInDict(args[0])
+			if viper.IsSet(args[0]) {
+				// Fetching definition from cache
+				var cache []Meaning
+				viper.UnmarshalKey(args[0], &cache)
+				for _, v := range cache {
+					fmt.Printf("%s%s%s\n", colors.Cyan, strings.ToUpper(v.POS), colors.Reset)
+					for _, def := range v.Definitions {
+						fmt.Printf("%s\u279C%s%s%s\n", colors.Blue, " ", colors.Reset, def.Definition)
+						if def.Example != "" {
+							fmt.Printf("%s\u2605%s%s%s\n", colors.Yellow, " ", colors.Reset, def.Example)
+						}
+					}
+					fmt.Println()
+				}
+			} else {
+				fetch(args[0])
+			}
 		}
 	},
 }
@@ -57,52 +63,29 @@ func init() {
 	rootCmd.AddCommand(searchCmd)
 }
 
-func searchInDict(word string) {
+func fetch(word string) {
 	var reqURL = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + word
 	res, err := http.Get(reqURL)
 	PrintErr(err)
 	defer res.Body.Close()
-	var result interface{}
 	body, err := ioutil.ReadAll(res.Body)
 	PrintErr(err)
 	if res.StatusCode == 200 {
+		var result []Body
 		err = json.Unmarshal(body, &result)
 		PrintErr(err)
-		var r = result.([]interface{})[0].(map[string]interface{})
-		var meaning = r["meanings"]
-		cacheSave := make(map[string][]interface{})
-
-		switch meaning := meaning.(type) {
-		case []interface{}:
-			for _, v := range meaning {
-				defs := v.(map[string]interface{})
-				keys := make([]string, 0)
-				for k, _ := range defs {
-					keys = append(keys, k)
-				}
-				sort.Strings(keys)
-				Reverse(keys)
-				pos := ""
-				for _, key := range keys {
-					if key == "partOfSpeech" {
-						posTemp := defs[key].(string)
-						fmt.Printf("%s%s%s\n", colors.Cyan, strings.ToUpper(posTemp), colors.Reset)
-						pos = posTemp
-					} else if key == "definitions" {
-						//fmt.Printf("%s\n\n", defs[key].([]interface{})[0].(map[string]interface{})["definition"])
-						for _, j := range defs[key].([]interface{}) {
-							defTemp := j.(map[string]interface{})["definition"]
-							example, exampleExists := j.(map[string]interface{})["example"]
-							fmt.Printf("%s\u279C%s%s%s\n", colors.Blue, " ", colors.Reset, defTemp)
-							cacheSave[pos] = append(cacheSave[pos], defTemp)
-							if exampleExists {
-								fmt.Printf("%s\u2605%s%s%s\n", colors.Yellow, " ", colors.Reset, example)
-								cacheSave[pos] = append(cacheSave[pos], "68f3fde1-8c1a-49eb-9f27-8d951b049142"+example.(string))
-							}
-						}
-						fmt.Println()
+		var cacheSave []Meaning
+		for _, val := range result {
+			cacheSave = val.Meanings
+			for _, meaning := range val.Meanings {
+				fmt.Printf("%s%s%s\n", colors.Cyan, strings.ToUpper(meaning.POS), colors.Reset)
+				for _, defs := range meaning.Definitions {
+					fmt.Printf("%s\u279C%s%s%s\n", colors.Blue, " ", colors.Reset, defs.Definition)
+					if defs.Example != "" {
+						fmt.Printf("%s\u2605%s%s%s\n", colors.Yellow, " ", colors.Reset, defs.Example)
 					}
 				}
+				fmt.Println()
 			}
 		}
 		wordListTemp := viper.GetStringSlice("wordList")
